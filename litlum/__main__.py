bd6614f3fbc54aa231d65b1dd968c94e0d7e4595
Nikolay Koldunov <koldunovn@gmail.com>
2025-06-01 15:10:19 +0200

    modify main

--- litlum/__main__.py
+++ litlum/__main__.py
@@ -4,10 +4,21 @@ import sys
 from litlum.ui.cli import CLI
 
 
-def main():
-    """Run the LitLum application."""
+def main(args=None):
+    """Run the LitLum application.
+    
+    Args:
+        args: Command line arguments. If None, uses sys.argv[1:]
+    """
+    if args is None:
+        args = sys.argv[1:]
+    
+    # If no arguments provided, show help
+    if not args:
+        args = ["--help"]
+        
     cli = CLI()
-    cli.run(sys.argv[1:])
+    cli.run(args)
 
 
 if __name__ == "__main__":
